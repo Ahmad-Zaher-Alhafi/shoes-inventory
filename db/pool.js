bd470857fe4cd4b9b70b42c020e15b2d76c664d2
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-12-05 21:17:55 +0300

    FIX: connection string of the data base pool was wrong

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,5 @@ const { Pool } = require("pg");
 // Again, this should be read from an environment variable
 module.exports = new Pool({
   connectionString:
-    "postgresql://shoes_inventory_owner:C3k6fUrHZoIa@ep-noisy-violet-a2qxy012.eu-central-1.aws.neon.tech/shoes_inventory?sslmode=require",
+    "postgresql://shoes_inventory_owner:C3k6fUrHZoIa@ep-wispy-salad-a2g0z9ld-pooler.eu-central-1.aws.neon.tech/shoes_inventory?sslmode=require",
 });
